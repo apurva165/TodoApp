47d29abe4213c45231248992b8e156d76f599b57
Apurva165 <Apurva165@gmail.com>
2017-05-07 15:55:17 -0500

    todo list app working

--- TodoApp/js/app.js
+++ TodoApp/js/app.js
@@ -1,4 +1,4 @@
-var item = [];
+var items = [];
 
 
 var itemList = document.getElementById('todo-list');
@@ -10,22 +10,49 @@ var itemInput = document.getElementById('todo-input');
 function addTodo() {
 	console.log('button is clicked');
 	// Get text from the input
-	item.push(itemInput.value);
-	console.log(item);
+	//var todoText = itemInput.value;
+	items.push(itemInput.value);
+	console.log(items);
+	itemInput.value='';
 	renderTodoList();
 }
 
 function renderTodoList() {
 	// clear the list 
-	itemList.innterHTML = " " ;
+	itemList.innerHTML = "" ;
 
 	// loopthrough items of array
-	for(var i = 0; i < item.length; i++) {
+	for(var i = 0; i < items.length; i++) {
 
 		var newTodo = document.createElement('li');
 		newTodo.id = 'item-' + i.toString();
-		newTodo.innerText = item[i];
+		newTodo.innerText = items[i];
+
+	// Options buttin  
+	appendOptionButtons(i , newTodo);
 
 		itemList.appendChild(newTodo);
 	}
 }
+
+
+function appendOptionButtons(index , item) {
+	//delete todo
+	var deleteButton = document.createElement('button');
+	deleteButton.className ='deleteBtn' ;
+
+	deleteButton.innerHTML = 'X';
+
+	deleteButton.onclick = function() {
+		removeTodo(index)
+	};
+	item.appendChild(deleteButton);
+}
+
+function removeTodo(index) {
+	items.splice(index ,1);
+	console.log(index)
+	renderTodoList();
+}
+
+
